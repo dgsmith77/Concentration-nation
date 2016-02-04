a927c8e50a76e08bcf5af203ee7c0086c8eebac4
Daniel Smith <dgsmith77@gmail.com>
2016-02-04 13:56:00 -0500

    fixed quotes
    Changed all double quotes to single quotes for consistency

--- JSConcentration/Scripts/Concentration/Concentration.js
+++ JSConcentration/Scripts/Concentration/Concentration.js
@@ -2,13 +2,13 @@
 
 var imgDir = 'images/';
 var defaultImg = 'face3.png';
-var picSet = ["Crab_icon.png", "Duck_icon.png", "Elephant_icon.png",
-    "Giraffe_icon.png", "Lion_icon.png", "Rabbit_icon.png",
-    "Rhino_icon.png", "Tiger_icon.png",
-    "Crab_icon.png", "Duck_icon.png", "Elephant_icon.png",
-    "Giraffe_icon.png", "Lion_icon.png", "Rabbit_icon.png",
-    "Rhino_icon.png", "Tiger_icon.png"];
-var scrambled = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+var picSet = ['Crab_icon.png', 'Duck_icon.png', 'Elephant_icon.png',
+    'Giraffe_icon.png', 'Lion_icon.png', 'Rabbit_icon.png',
+    'Rhino_icon.png', 'Tiger_icon.png',
+    'Crab_icon.png', 'Duck_icon.png', 'Elephant_icon.png',
+    'Giraffe_icon.png', 'Lion_icon.png', 'Rabbit_icon.png',
+    'Rhino_icon.png', 'Tiger_icon.png'];
+var scrambled = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
 var clicks = 0;
 var uncovered = 0;
 var moves = 0;
@@ -37,11 +37,11 @@ Concentration.gameFunctions = (function ($) {
         var buttonIndex = 0;
         for (i = 0; i < 4; i++) {
             for (j = 0; j < 4; j++) {
-                var button = $('<input class="gameButton" />').attr({ type: 'image', id: buttonIndex, src: imgDir + defaultImg });
-                $("#gameButtons").append(button);
+                var button = $('<input class=\'gameButton\' />').attr({ type: 'image', id: buttonIndex, src: imgDir + defaultImg });
+                $('#gameButtons').append(button);
                 buttonIndex++;
             }
-            $("#gameButtons").append("<br>");
+            $('#gameButtons').append('<br>');
         }
     },
     scrambleImages = function () {
@@ -49,7 +49,7 @@ Concentration.gameFunctions = (function ($) {
         do
         {
             var j = Math.floor(Math.random() * 16);
-            if (scrambled[j] == "")
+            if (scrambled[j] == '')
             {
                 scrambled[j] = picSet[i];
                 i++;
@@ -73,34 +73,34 @@ Concentration.gameFunctions = (function ($) {
             }
             prevIndex = index;
         }
-		// check for complete gameButton
-		if(uncovered == picSet.length) {
+	    // check for complete gameButton
+	    if(uncovered == picSet.length) {
 		    var msg = 'You took ' + moves + ' moves to complete the puzzle.\n\n';
-			switch(true) {
-				case(moves <= 13):
+		    switch(true) {
+			    case(moves <= 13):
 				    msg += 'Brilliant!!';
-					break;
-				case(moves > 13 && moves <= 15):
-					msg += 'Well done!';
-					break;
-				case(moves > 15 && moves <= 18):
-					msg += 'Nice job.';
-					break;
-				case(moves > 18 && moves <= 21):
-					msg += 'Not bad.';
-					break;
-				case(moves > 21 && moves <= 24):
-					msg += 'Well...we can\'t all be geniuses.';
-					break;
-				case(moves > 24):
-					msg += 'You might want to get checked for Alzheimers.';
-					break;
-			}
-			alert(msg);
-		}
+				    break;
+			    case(moves > 13 && moves <= 15):
+				    msg += 'Well done!';
+				    break;
+			    case(moves > 15 && moves <= 17):
+				    msg += 'Nice job.';
+				    break;
+			    case(moves > 17 && moves <= 19):
+				    msg += 'Not bad.';
+				    break;
+			    case(moves > 19 && moves <= 21):
+				    msg += 'Well...we can\'t all be geniuses.';
+				    break;
+			    case(moves > 21):
+				    msg += 'You might want to get checked for Alzheimers.';
+				    break;
+		    }
+		    alert(msg);
+	    }
     },
     newGame = function () {
-        scrambled = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+        scrambled = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
         clicks = 0;
         uncovered = 0;
         moves = 0;
@@ -109,7 +109,7 @@ Concentration.gameFunctions = (function ($) {
         $('.gameButton').each(function () {
             $(this).attr('src', imgDir + defaultImg);
         });
-        $('#moves').text("0 Moves");
+        $('#moves').text('0 Moves');
     },
     doPause = function (miliseconds, curr, prev) {
         setTimeout(function () {
